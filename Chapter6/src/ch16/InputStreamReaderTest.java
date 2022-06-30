cf9d8a67e50a1a3ca51ce3e7ef093e024d59203c
PNS212624\Hansol <yrlee00@hansol.com>
2022-06-30 16:15:41 +0900

    6/30 commit

---
+++ Chapter6/src/ch16/InputStreamReaderTest.java
@@ -0,0 +1,23 @@
+package ch16;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReaderTest {
+
+	public static void main(String[] args) {
+
+		try(InputStreamReader isr = new InputStreamReader(new FileInputStream("reader.txt"))){
+			int i;
+			
+			while( (i = isr.read()) != -1) {
+				System.out.println((char)i);
+			}
+		} catch(IOException e) {
+			
+		}
+		
+	}
+
+}
