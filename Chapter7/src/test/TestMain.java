038b270c033fd2613b451bdd7dbe3eba11f006fa
PNS212624\Hansol <yrlee00@hansol.com>
2022-07-04 18:00:36 +0900

    7/4 commit

--- Chapter7/src/test/TestMain.java
+++ Chapter7/src/test/TestMain.java
@@ -1,10 +1,21 @@
 package test;
 
+
+import school.School;
+import school.Subject;
+
 public class TestMain {
+	
+	School goodSchool = School.getInstance();
+	Subject korean;
+	Subject math;
+	
+	GenerateGradeReport gradeReport = new GenerateGradeReport();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		
 	}
 
 }
